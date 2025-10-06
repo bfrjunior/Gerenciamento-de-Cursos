960966c9d37d4563008d3c67358e65977cc42041
Fernando Rocha <beneditofernandor@gmail.com>
2025-10-06 10:33:45 -0300

    criando as entidades

---
+++ Gerenciamento-cursos/Model/CursoModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Gerenciamento_cursos.Model
+{
+    public class CursoModel
+    {
+
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Descricao { get; set; }
+
+        public ICollection<MatriculaModel> Matriculas { get; set; } = new List<MatriculaModel>();
+    }
+}
---
+++ Gerenciamento-cursos/Model/MatriculaModel.cs
@@ -0,0 +1,20 @@
+﻿namespace Gerenciamento_cursos.Model
+{
+    public class MatriculaModel
+    {
+
+        public int AlunoId { get; set; }
+
+
+        public AlunoModel Aluno { get; set; }
+
+
+        public int CursoId { get; set; }
+
+
+        public CursoModel Curso { get; set; }
+
+
+        public DateTime DataMatricula { get; set; } = DateTime.Now;
+    }
+}
