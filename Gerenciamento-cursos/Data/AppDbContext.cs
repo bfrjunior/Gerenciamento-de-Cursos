d36aa38a61d8a990695b547ec67f04c2829fd13d
Fernando Rocha <beneditofernandor@gmail.com>
2025-10-06 12:32:16 -0300

    configurando contexto de conexão

--- Gerenciamento-cursos/Data/AppDbContext.cs
+++ Gerenciamento-cursos/Data/AppDbContext.cs
@@ -1,6 +1,38 @@
-﻿namespace Gerenciamento_cursos.Data
+﻿using Gerenciamento_cursos.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gerenciamento_cursos.Data
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        // DbSets para suas entidades
+        public DbSet<AlunoModel> Alunos { get; set; }
+        public DbSet<CursoModel> Cursos { get; set; }
+        public DbSet<MatriculaModel> Matriculas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<MatriculaModel>()
+                .HasKey(m => new { m.AlunoId, m.CursoId });
+
+
+            modelBuilder.Entity<MatriculaModel>()
+                .HasOne(m => m.Aluno)
+                .WithMany(a => a.Matriculas)
+                .HasForeignKey(m => m.AlunoId);
+
+
+            modelBuilder.Entity<MatriculaModel>()
+                .HasOne(m => m.Curso)
+                .WithMany(c => c.Matriculas)
+                .HasForeignKey(m => m.CursoId);
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
